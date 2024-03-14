6a565957d6d82906b38f9d36bb9af8c9537b4c27
kpodlaski <podlaski@uni.lodz.pl>
2024-03-14 14:50:44 +0100

    14.03.2024

---
+++ czwartek/MultithreadIntro/Race/BarrierTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Race
+{
+    class BarrierTest
+    {
+        Barrier barrier;
+
+        public BarrierTest(Barrier b)
+        {
+            this.barrier = b;
+        }
+
+        public void Start()
+        {
+            Thread t = new Thread(new ThreadStart(Run));
+            t.Start();
+        }
+
+        private void Run()
+        {
+            Console.WriteLine("Dochodzę do bariery");
+            barrier.SignalAndWait();
+            Console.WriteLine("Koniec pracy");
+        }
+    }
+}
---
+++ czwartek/MultithreadIntro/Race/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Race
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            SpeedwayRace race = new SpeedwayRace(4, 30000);
+            race.StartRace(new string[] { "Adam", "Marian", "Zenon", "Kamil" });
+        }
+
+
+        static void Main2(string[] a)
+        {
+            Barrier b = new Barrier(2, (b) => { 
+                Thread.Sleep(2000); 
+                Console.WriteLine("Zakończono metodę bariery"); 
+            });
+
+            BarrierTest b1 = new BarrierTest(b);
+            b1.Start();
+            b1 = new BarrierTest(b);
+            b1.Start();
+        }
+
+    }
+}
