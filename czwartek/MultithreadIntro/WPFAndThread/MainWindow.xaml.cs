4364016946e0bbe7af8f08334399834e79718a07
kpodlaski <podlaski@uni.lodz.pl>
2024-03-14 17:37:49 +0100

    14.03.2024_v3

---
+++ czwartek/MultithreadIntro/WPFAndThread/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAndThread
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(animate));
+            t.Start();
+        }
+        private void animate()
+        {
+
+            Debug.WriteLine("Kliknięto klawisz th.ID:{0}",
+                           Thread.CurrentThread.ManagedThreadId);
+            double x = 110;
+            double y = 73;
+            while (x < 800 && y < 480)
+            {
+                x += 1;
+                y += 1;
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    Canvas.SetLeft(Ball, x);
+                    Canvas.SetTop(Ball, y);
+                    if (x < 200 && x > 199.98)
+                    {
+                        Debug.WriteLine("z invoke th.ID:{0}",
+                            Thread.CurrentThread.ManagedThreadId
+                            );
+                    }
+                }));
+                Thread.Sleep(10);
+            }
+            Debug.WriteLine("Koniec animacji th.ID:{0}",
+                           Thread.CurrentThread.ManagedThreadId
+                           );
+        }
+    }
+}
